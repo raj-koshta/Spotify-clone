f5b87c978d0f533ce8d2ce72552de5003f69141f
raj-koshta <rajkoshta.rk1@gmail.com>
2024-11-29 21:45:04 +0530

    changes in AlbumItems

--- src/components/AlbumItems.jsx
+++ src/components/AlbumItems.jsx
@@ -6,10 +6,10 @@ const AlbumItems = ({image,name,desc,id}) => {
   const navigate = useNavigate()
 
   return (
-    <div onClick={()=>navigate(`/album/${id}`)} className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff]'>
+    <div onClick={()=>navigate(`/album/${id}`)} className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#787777] hover:text-black'>
         <img className='rounded' src={image} alt=''/> 
         <p className='font-bold mt-2 mb-1'>{name}</p>
-        <p className='text-slate-200 text-sm'>{desc}</p>
+        <p className='text-slate-200 text-sm hover:text-black'>{desc}</p>
     </div>
   )
 }
